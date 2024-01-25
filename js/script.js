49998c88086eb2f855d3704a9e5e6dc0f2b02391
mitchredd77 <147752683+mitchredd77@users.noreply.github.com>
2024-01-24 17:38:00 -0700

    Fixed search feature

--- js/script.js
+++ js/script.js
@@ -15,24 +15,57 @@ searchInput.addEventListener("keyup", () => {
    const searchString = searchInput.value.toLowerCase();
    for (i = 0; i < data.length; i++) {
      const student_last = data[i].name.last.toLowerCase();
+     
+     
      const student_first = data[i].name.first.toLowerCase();
+     
      if (student_last.includes(searchString) || student_first.includes(searchString)) {
        newData.push(data[i]);
+       console.log(student_first);
+       console.log(student_last);
+       console.log(newData);
      }
      }
      if (newData.length > 0) {
-      console.log(newData.length);
        addPagination(newData);
        showPage(newData, 1);
      } else {
        studentContainer.innerHTML = "<h3>No results were found.</h3>";
-       studentContainer.innerHTML = html;
+       studentContainer.innerHTML = studentTemplate;
        paginationList.innerHTML = "";
      }
  });
 
- //Displays page with correct number of students on page
-function showPage(list, page) {
+
+
+// Creates buttons for each page
+function addPagination(list) {
+  const buttons = Math.ceil(list.length / studentsPerPage);
+  paginationList.innerHTML = '';
+  for (let i=1; i <= buttons; i++) {
+    const buttonTemplate = `<li>
+    <button>${i}</button>
+  </li>
+  `;
+  paginationList.insertAdjacentHTML("beforeend", buttonTemplate);
+}
+paginationList.querySelector("button").classList.add("active");
+//Adding Listener for navigation buttons
+paginationList.addEventListener("click", (e) => {
+   const activeButton = document.querySelector(".link-list button.active");
+ 
+   if (e.target.tagName === "BUTTON") {
+      activeButton.classList.remove("active");
+      e.target.classList.add("active");
+      showPage(list, parseInt(e.target.textContent));
+   }
+ });
+}
+
+
+
+ //Displays page and dynamically adds students to each page
+ function showPage(list, page) {
    const startStudent = (page * studentsPerPage) - studentsPerPage;
    const lastStudent = (page * studentsPerPage) - 1;
    studentContainer.innerHTML = '';
@@ -48,36 +81,11 @@ function showPage(list, page) {
        <div class="joined-details">
          <span class="date">${student.registered.date}</span>
        </div>
-     </li>`
+     </li>`;
      studentContainer.insertAdjacentHTML("beforeend", studentTemplate);
    }
-   }
+  }
  }
-
-// Creates buttons for each page
-function addPagination(list) {
-  const buttons = Math.ceil(list.length / studentsPerPage);
-  paginationList.innerHTML = '';
-  for (let i=1; i <= buttons; i++) {
-    const buttonTemplate = `<li>
-    <button>${i}</button>
-  </li>
-  `;
-  paginationList.insertAdjacentHTML("beforeend", buttonTemplate);
-}
-paginationList.querySelector("button").classList.add("active");
-}
-
-//Adding Listener for navigation buttons
-paginationList.addEventListener("click", (e) => {
-   const activeButton = document.querySelector(".link-list button.active");
- 
-   if (e.target.tagName === "BUTTON") {
-      activeButton.classList.remove("active");
-      e.target.classList.add("active");
-      showPage(data, parseInt(e.target.textContent));
-   }
- });
 
 // Call functions
 addPagination(data);
